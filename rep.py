cd1ee0e671415eb61ade39194f27bd477069797d
blasty <blasty@fail0verflow.com>
2013-10-22 14:17:21 +0200

    repeater module

--- rep.py
+++ rep.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
+import sys
+
 def main(args):
 	if len(args) != 2:
 		print "usage: moneyshot rep <str> <length>"
 		return
 
-	print args[0] * int(args[1]),
+	sys.stdout.write(args[0] * int(args[1]))
