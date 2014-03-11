f419c45fcdfae18a5af4c59c0796edd0f35ca9a1
blasty <blasty@fail0verflow.com>
2014-03-11 19:40:35 +0100

    add STDIN reading for dwords helper

--- dwords.py
+++ dwords.py
@@ -4,6 +4,18 @@ import struct
 import sys
 
 def main(args):
+	# try read STDIN with 0 argz
+	if len(args) == 0:
+		lines = sys.stdin.readlines()
+
+		args = []
+
+		for line in lines:
+			parts = line.split(" ")
+
+			for part in parts:
+				args.append(part)
+
 	if len(args) == 0:
 		print "usage: moneyshot rep dwords <dword1> [dword2] [dword..]"
 		return
